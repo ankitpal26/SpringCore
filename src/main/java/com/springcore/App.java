75c1fb0c233f800e353aeebbc5421017d4d95f36
Ankit <ankitpal28090016@gmail.com>
2023-09-02 17:48:13 +0530

    to inject emp class through context object

--- src/main/java/com/springcore/App.java
+++ src/main/java/com/springcore/App.java
@@ -13,9 +13,16 @@ public class App
     {
         System.out.println( "Hello World!" );
         ApplicationContext context = new ClassPathXmlApplicationContext("studentconfig.xml");
+        ApplicationContext context2 = new ClassPathXmlApplicationContext("empconfig.xml");
+        
         Student student1 =(Student) context.getBean("student1");
-        Student student2 =(Student) context.getBean("student2");
+        Emp emp =(Emp) context2.getBean("emp1");
+//        Student student2 =(Student) context.getBean("student2");
         System.out.println(student1);
-        System.out.println(student2);
+        System.out.println(emp);
+//        System.out.println(student2);
+        
+        
+        
     }
 }
