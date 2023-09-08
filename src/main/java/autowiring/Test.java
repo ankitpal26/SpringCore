14fb0902ca59ca4fff6a2a27757e24184fae768d
Ankit <ankitpal28090016@gmail.com>
2023-09-08 18:57:24 +0530

    autowing uses through xml

---
+++ src/main/java/autowiring/Test.java
@@ -0,0 +1,18 @@
+package autowiring;
+
+
+import javax.naming.Context;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+	ApplicationContext context = new ClassPathXmlApplicationContext("autowiring/autoconfig.xml");
+    Emp emp1 =	context.getBean("emp1",Emp.class);
+	System.out.println(emp1);
+	}
+        
+}
